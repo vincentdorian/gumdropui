bad75741916af969930988f01ac2d2ff48795707
vincentdorian <hello@vincentdorian.me>
2023-02-05 13:41:39 +0100

    gumdrop shadow

--- dist/base.js
+++ dist/base.js
@@ -2,7 +2,8 @@ module.exports = {
     ":root": {
         "--color-gum-shadow": "#000",
         "--color-gum-shadow-border": "#000",
-        "--width-gum-shadow-border": 0,
+        "--width-gum-shadow-border": "0px",
+        "--size-gum-shadow-border": "0px",
         "--size-gum-shadow": 0,
     }
 }
--- dist/utilities.js
+++ dist/utilities.js
@@ -3,41 +3,53 @@ module.exports = {
         "box-shadow": "0 0 0 0 theme('colors.gum-shadow'), 0 0 0 0 theme('colors.gum-shadow-border')"
     },
     ".gum-shadow-xs": {
-        "--size-gum-shadow": "1px",
-        "box-shadow": "var(--size-gum-shadow) var(--size-gum-shadow) 0 var(--width-gum-shadow-border) theme('colors.gum-shadow'), var(--size-gum-shadow) var(--size-gum-shadow) 0 0 theme('colors.gum-shadow-border')"
+        "--size-gum-shadow": "0.06125rem",
+        "box-shadow": "var(--size-gum-shadow) var(--size-gum-shadow) 0 calc(var(--width-gum-shadow-border)*(-1)) theme('colors.gum-shadow'), var(--size-gum-shadow-border) var(--size-gum-shadow-border) 0 0 theme('colors.gum-shadow-border')"
     },
     ".gum-shadow-sm": {
-        "--size-gum-shadow": "2px",
-        "box-shadow": "var(--size-gum-shadow) var(--size-gum-shadow) 0 var(--width-gum-shadow-border) theme('colors.gum-shadow'), var(--size-gum-shadow) var(--size-gum-shadow) 0 0 theme('colors.gum-shadow-border')"
+        "--size-gum-shadow": "0.125rem",
+        "box-shadow": "var(--size-gum-shadow) var(--size-gum-shadow) 0 calc(var(--width-gum-shadow-border)*(-1)) theme('colors.gum-shadow'), var(--size-gum-shadow-border) var(--size-gum-shadow-border) 0 0 theme('colors.gum-shadow-border')"
     },
     ".gum-shadow": {
-        "--size-gum-shadow": "3px",
-        "box-shadow": "var(--size-gum-shadow) var(--size-gum-shadow) 0 var(--width-gum-shadow-border) theme('colors.gum-shadow'), var(--size-gum-shadow) var(--size-gum-shadow) 0 0 theme('colors.gum-shadow-border')"
+        "--size-gum-shadow": "0.25rem",
+        "box-shadow": "var(--size-gum-shadow) var(--size-gum-shadow) 0 calc(var(--width-gum-shadow-border)*(-1)) theme('colors.gum-shadow'), var(--size-gum-shadow-border) var(--size-gum-shadow-border) 0 0 theme('colors.gum-shadow-border')"
     },
     ".gum-shadow-md":{
-        "--size-gum-shadow": "5px",
-        "box-shadow": "var(--size-gum-shadow) var(--size-gum-shadow) 0 var(--width-gum-shadow-border) theme('colors.gum-shadow'), var(--size-gum-shadow) var(--size-gum-shadow) 0 0 theme('colors.gum-shadow-border')"
+        "--size-gum-shadow": "0.375rem",
+        "box-shadow": "var(--size-gum-shadow) var(--size-gum-shadow) 0 calc(var(--width-gum-shadow-border)*(-1)) theme('colors.gum-shadow'), var(--size-gum-shadow-border) var(--size-gum-shadow-border) 0 0 theme('colors.gum-shadow-border')"
     },
     ".gum-shadow-lg": {
-        "--size-gum-shadow": "7px",
-        "box-shadow": "var(--size-gum-shadow) var(--size-gum-shadow) 0 var(--width-gum-shadow-border) theme('colors.gum-shadow'), var(--size-gum-shadow) var(--size-gum-shadow) 0 0 theme('colors.gum-shadow-border')",
+        "--size-gum-shadow": "0.5rem",
+        "box-shadow": "var(--size-gum-shadow) var(--size-gum-shadow) 0 calc(var(--width-gum-shadow-border)*(-1)) theme('colors.gum-shadow'), var(--size-gum-shadow-border) var(--size-gum-shadow-border) 0 0 theme('colors.gum-shadow-border')"
     },
     ".gum-shadow-xl": {
-        "--size-gum-shadow": "9px",
-        "box-shadow": "var(--size-gum-shadow) var(--size-gum-shadow) 0 var(--width-gum-shadow-border) theme('colors.gum-shadow'), var(--size-gum-shadow) var(--size-gum-shadow) 0 0 theme('colors.gum-shadow-border')",
+        "--size-gum-shadow": "0.75rem",
+        "box-shadow": "var(--size-gum-shadow) var(--size-gum-shadow) 0 calc(var(--width-gum-shadow-border)*(-1)) theme('colors.gum-shadow'), var(--size-gum-shadow-border) var(--size-gum-shadow-border) 0 0 theme('colors.gum-shadow-border')"
     },
     ".gum-shadow-2xl": {
-        "--size-gum-shadow": "12px",
-        "box-shadow": "var(--size-gum-shadow) var(--size-gum-shadow) 0 var(--width-gum-shadow-border) theme('colors.gum-shadow'), var(--size-gum-shadow) var(--size-gum-shadow) 0 0 theme('colors.gum-shadow-border')"
+        "--size-gum-shadow": "1rem",
+        "box-shadow": "var(--size-gum-shadow) var(--size-gum-shadow) 0 calc(var(--width-gum-shadow-border)*(-1)) theme('colors.gum-shadow'), var(--size-gum-shadow-border) var(--size-gum-shadow-border) 0 0 theme('colors.gum-shadow-border')"
     },
-    ".gum-shadow-border-1": {
-        "--width-gum-shadow-border": "-1px"
+    ".gum-shadow-border-0": {
+        "--width-gum-shadow-border": "0px",
+        "--size-gum-shadow-border": "var(--size-gum-shadow)"
+    },
+    ".gum-shadow-border": {
+        "--width-gum-shadow-border": "1px",
+        "--size-gum-shadow-border": "var(--size-gum-shadow)"
     },
     ".gum-shadow-border-2": {
-    "--width-gum-shadow-border": "-2px",
+        "--width-gum-shadow-border": "2px",
+        "--size-gum-shadow-border": "var(--size-gum-shadow)"
     },
     ".gum-shadow-border-3": {
-        "--width-gum-shadow-border": "-3px",
+        "--width-gum-shadow-border": "3px",
+        "--size-gum-shadow-border": "var(--size-gum-shadow)"
+    },
+    
+    ".gum-shadow-border-4": {
+        "--width-gum-shadow-border": "4px",
+        "--size-gum-shadow-border": "var(--size-gum-shadow)"
     },
     ".gum-float": {
         "transform": "translate(calc(var(--size-gum-shadow) * -1 ), calc(var(--size-gum-shadow) * -1 ))",
